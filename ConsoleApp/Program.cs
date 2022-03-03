d18825bc5376cff629c779944736530e05db9576
Yusufboy Atajanov <Yusufboy_Atajanov@epam.com>
2022-03-03 19:30:00 +0500

    console initial

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,6 +9,14 @@ namespace ConsoleApp
             Console.Write("What is your name ? : ");
             string name = Console.ReadLine();   
             Console.WriteLine($"Hello,{name}!");
+
+            string n = "";
+            string s = "absdefghijklmnbopqestuvg";
+            foreach ( char i in s)
+            {
+                n += i;
+            }
+            throw new AccessViolationException();
         }
     }
 }
